88d926617ddac6a80df727d5693eee87ab06c260
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2025-06-18 11:51:36 +0500

    Commit working:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <div>
+        bOOOYAAA
+      </div>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
