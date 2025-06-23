6ed167e759c70775893895c1c8a7f0c502be9b38
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2025-06-23 11:42:20 +0500

    Feat: add guardian code for if invalid credential or the login is not successful

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,6 @@
 // import { createSession } from '@/lib/session/session';
 //import { LoginSchema } from '@/lib/validation/loginSchema';
+import { LoginSchema } from '@/lib/validation/loginSchema';
 import { NextResponse } from 'next/server';
 
 const testUser = {
@@ -13,26 +14,25 @@ export async function POST(req: Request) {
     const body = await req.json();
     console.log('Received body:', body);
 
+    const result = LoginSchema.safeParse(body)
+
+    if (!result.success) {
+     return NextResponse.json({
+      errors: result.error.flatten().fieldErrors,
+    }, { status: 400 });
+    }
+
     const { email, password} = body;
-
-    if (!email || !password) {
-      return NextResponse.json({ message: 'Missing credentials' }, { status: 400 });
-    }
-
-    if (email !== testUser.email || password !== testUser.password) {
-      return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
-    }
-
+    
     if (email == testUser.email && password == testUser.password) {
       return NextResponse.json({ id: testUser.id });
     }
+    else {
+      return NextResponse.json({errors: {
+        email: ["Invalid email or password"],
+      }}, {status: 401})
+    }
 
-
-    const res = NextResponse.json({ message: 'Login successful' });
-
-    
-
-    return res;
   } catch (err) {
     console.error('API error:', err);
     return NextResponse.json({ message: 'Server error' }, { status: 500 });
