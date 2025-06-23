2aa4d6756565b6aa0889d385b3b7cf099071ee60
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2025-06-23 10:50:50 +0500

    Feat: Add login validation as well as verification logic to route

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,11 +1,41 @@
+// import { createSession } from '@/lib/session/session';
+//import { LoginSchema } from '@/lib/validation/loginSchema';
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
-  const { username, password } = await request.json();
+const testUser = {
+  id: "1",
+  email: "admin@gmail.com",
+  password: "12345678",
+};
 
-  if (username === 'admin' && password === 'password123') {
-    return NextResponse.json({ message: 'Login successful' });
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    console.log('Received body:', body);
+
+    const { email, password} = body;
+
+    if (!email || !password) {
+      return NextResponse.json({ message: 'Missing credentials' }, { status: 400 });
+    }
+
+    if (email !== testUser.email || password !== testUser.password) {
+      return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    }
+
+    if (email == testUser.email && password == testUser.password) {
+      return NextResponse.json({ id: testUser.id });
+    }
+
+
+    const res = NextResponse.json({ message: 'Login successful' });
+
+    
+
+    return res;
+  } catch (err) {
+    console.error('API error:', err);
+    return NextResponse.json({ message: 'Server error' }, { status: 500 });
   }
 
-  return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
 }
--- src/lib/validation/loginSchema.ts
+++ src/lib/validation/loginSchema.ts
@@ -4,3 +4,5 @@ export const LoginSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid email"),
   password: z.string().min(1, "Password is required"),
 });
+
+export type LoginInput = z.infer<typeof LoginSchema>;
